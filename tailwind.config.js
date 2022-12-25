db49ee4bef671825c6e0bff0e13cdbff68500db3
mertaseptiawan <mertaseptiawan@gmail.com>
2022-12-25 16:08:15 +0800

    add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
