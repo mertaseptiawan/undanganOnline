5c34da26a0a95d58144c384344adc5bacba6058c
mertaseptiawan <mertaseptiawan@gmail.com>
2023-01-11 10:24:17 +0800

    initial commit

---
+++ theme2/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dancing: "'Great Vibes', cursive",
+      },
+
+      colors: {
+        countdown: '#DCDCDC',
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ theme3/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dancing: "'Passions Conflict', cursive",
+      },
+
+      colors: {
+        countdown: '#DCDCDC',
+      },
+    },
+  },
+  plugins: [],
+}
